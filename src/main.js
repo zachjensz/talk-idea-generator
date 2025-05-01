c3dc9b2ebbf9ecad9c655730549dfb722296ca5f
Zach Jensz <zach@zachjensz.com>
2025-05-01 23:27:07 +1000

    Choose from a pool of ideas at random

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 const form = document.querySelector('form')
 const output = document.querySelector('#output')
 
+const ideas = [
+  "Juniors should focus on HTML, CSS and JS instead of frameworks, here's why",
+  "React is terrible and it ruined my day",
+  "View Transitions and speculative rendering: why MPAs have a comeback",
+  "New Map and Set methods in JS",
+  "You need to try CSS container queries",
+  "How to not spend $10,000 on AWS",
+  "I tried writing my own JS framework so you don't have to",
+  "CSS rem vs em",
+  "Is Tailwind really that bad? Yes.",
+  "5 HTML elements you should likely know but don't",
+  "HTML popovers vs dialogs",
+  "Can I hear your website?",
+  "Should you generate alt text with AI?",
+  "How does AI change the a11y landscape?",
+  "Do we have too many web APIs?",
+  "Why aren't PWAs more popular?",
+  "How to start a local tech meetup",
+  "How to give an engaging tech talk",
+  "Do you really know the CSS cascade?"
+]
+
+const chooseAtRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
 form.onsubmit = (e) => {
   e.preventDefault();
-  output.innerText = "You have good ideas in there, use your brain"
+  output.innerText = chooseAtRandom(ideas)
 }
